d7e6ad3230735982d99e3083b1dccd94bae46787
javierdevegaruiz <javier.devega.ruiz@gmail.com>
2016-05-01 18:49:51 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Suave.Serilog
 Get your Suave logs onto Serilog
+
+## How to use
+```fsharp
+  startWebServer { defaultConfig with logger = Suave.Serilog.LogAdapter Serilog.Log.Logger } app
+```
---
+++ src/Suave.Serilog/SuaveSerilogAdapter.fs
@@ -0,0 +1,21 @@
+﻿namespace Suave.Serilog
+
+open Suave.Logging
+open Serilog.Events
+
+type LogAdapter (logger : Serilog.ILogger) =
+    let mapLevel = function
+    | LogLevel.Debug -> LogEventLevel.Debug
+    | LogLevel.Error -> LogEventLevel.Error
+    | LogLevel.Fatal -> LogEventLevel.Fatal
+    | LogLevel.Info -> LogEventLevel.Information
+    | LogLevel.Verbose -> LogEventLevel.Verbose
+    | LogLevel.Warn -> LogEventLevel.Warning
+        
+    interface Logger with
+        member x.Log level fLine =
+            let line = fLine ()
+            let serilogLevel = mapLevel level
+            match line.``exception`` with
+            | Some exn -> logger.Write (serilogLevel, exn, line.message)
+            | None -> logger.Write (serilogLevel, line.message)
